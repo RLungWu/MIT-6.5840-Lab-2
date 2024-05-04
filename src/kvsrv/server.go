e40ed07bfdb304b36fdda28326f2edc626710255
RLungWu <larry14789@gmail.com>
2024-05-05 02:19:30 +0800

    Basic Implement

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -19,25 +19,52 @@ type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	data map[string]string
 }
 
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	value, ok := kv.data[args.Key]
+	if ok {
+		reply.Value = value
+	}
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	_, ok := kv.data[args.Key]
+	if ok{
+		kv.data[args.Key] = args.Value
+	}else{
+		kv.data[args.Key] = args.Value
+	}
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	value, ok := kv.data[args.Key]
+	if ok{
+		kv.data[args.Key] = value + args.Value
+	}else{
+		kv.data[args.Key] = args.Value
+	}
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
 	// You may need initialization code here.
+	kv.data = make(map[string]string)
 
 	return kv
 }
