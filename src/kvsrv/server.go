57e5933bc040f3d9cc621f7c2e59446f281ac926
RLungWu <larry14789@gmail.com>
2024-05-06 23:45:08 +0800

    update server

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,14 +14,13 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
-	data map[string]string
+	data       map[string]string
+	requestIDs sync.Map
 }
-
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
@@ -36,35 +35,59 @@ func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
-	kv.mu.Lock()
-	defer kv.mu.Unlock()
+	if args.Mode == Mode_Report {
+		kv.requestIDs.Delete(args.RequestID)
+		return
+	}
 
-	_, ok := kv.data[args.Key]
-	if ok{
-		kv.data[args.Key] = args.Value
-	}else{
+	v, ok := kv.requestIDs.Load(args.RequestID)
+	if ok {
+		reply.Value = v.(string)
+		return
+	}
+
+	kv.mu.Lock()
+	old := kv.data[args.Key]
+	if old != args.Value {
 		kv.data[args.Key] = args.Value
 	}
+	kv.mu.Unlock()
+
+	reply.Value = old
+	kv.requestIDs.Store(args.RequestID, old)
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
-	kv.mu.Lock()
-	defer kv.mu.Unlock()
-
-	value, ok := kv.data[args.Key]
-	if ok{
-		kv.data[args.Key] = value + args.Value
-	}else{
-		kv.data[args.Key] = args.Value
+	if args.Mode == Mode_Report {
+		kv.requestIDs.Delete(args.RequestID)
+		return
 	}
+
+	v, ok := kv.requestIDs.Load(args.RequestID)
+	if ok {
+		reply.Value = v.(string)
+		return
+	}
+
+	kv.mu.Lock()
+	old := kv.data[args.Key]
+	if old == "" {
+		kv.data[args.Key] = args.Value
+	} else {
+		kv.data[args.Key] = old + args.Value
+	}
+	kv.mu.Unlock()
+
+	reply.Value = old
+	kv.requestIDs.Store(args.RequestID, old)
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
 	// You may need initialization code here.
-	kv.data = make(map[string]string)
+	kv.data = make(map[string]string, 20)
 
 	return kv
 }
